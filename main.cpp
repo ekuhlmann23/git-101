d692d8e9d02f231fe28d0c459e28eb9160f46c50
Erik Kühlmann Salazar <ekuhlmann23@gmail.com>
2021-10-23 15:32:26 -0600

    Add user greeting

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
 
 int main() {
-	std::cout << "Hello world!\n";
+	std::string name;
+	std::cin >> name;
+	std::cout << "Hello there, " << name << std::endl;
 }
