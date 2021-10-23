40de9f7eba916e9cdc1b73cc3b03068fa50f12bf
ekuhlmann23 <48370687+ekuhlmann23@users.noreply.github.com>
2021-10-23 15:56:43 -0600

    Add documentation and return value.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 
+// This is the main function.
 int main() {
 	std::cout << "Please type your name: ";
 	std::string name;
 	std::cin >> name;
 	std::cout << "Hello there, " << name << "!!!" << std::endl;
+	
+	return 0;
 }
